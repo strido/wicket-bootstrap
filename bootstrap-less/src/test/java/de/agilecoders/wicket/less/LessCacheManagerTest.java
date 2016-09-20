2be0d8d4294162bd103e63c74198d90457e6069e
Erik Strid <erik.strid@idainfront.se>
2016-09-20 13:59:13 +0200

    Tests for the LessCacheManager

--- bootstrap-less/src/test/java/de/agilecoders/wicket/less/LessCacheManagerTest.java
+++ bootstrap-less/src/test/java/de/agilecoders/wicket/less/LessCacheManagerTest.java
@@ -1,17 +1,18 @@
 package de.agilecoders.wicket.less;
 
-import static org.junit.Assert.*;
-
+import org.apache.wicket.util.time.Time;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.github.sommeri.less4j.LessSource;
+import static org.junit.Assert.assertEquals;
+
 import com.github.sommeri.less4j.LessCompiler.Configuration;
+import com.github.sommeri.less4j.LessSource;
 import com.github.sommeri.less4j.LessSource.URLSource;
 
 
 public class LessCacheManagerTest {
-    
+
     private int invocationOfGetContent;
     private int invocationOfNewConfiguration;
 
@@ -37,15 +38,15 @@ public class LessCacheManagerTest {
     @Test
     public void cachesCssResult() {
         LessCacheManager cacheManager = new LessCacheManager();
-        
+
         URLSource urlSource = createSampleURLSource();
-        
+
         // LessSource.getContent() ist only necessary, when compiling the .less file.
         // Otherwise the result would be in the cache.
-        
+
         cacheManager.getCss(urlSource);
         assertEquals(1, invocationOfGetContent);
-        
+
         cacheManager.getCss(urlSource);
         assertEquals(1, invocationOfGetContent);
     }
@@ -53,21 +54,56 @@ public class LessCacheManagerTest {
     @Test
     public void clearCacheForcesRecompile() {
         LessCacheManager cacheManager = new LessCacheManager();
-        
+
         URLSource urlSource = createSampleURLSource();
-        
+
         // LessSource.getContent() ist only necessary, when compiling the .less file.
         // Otherwise the result would be in the cache.
-        
+
         cacheManager.getCss(urlSource);
         assertEquals(1, invocationOfGetContent);
-        
+
         cacheManager.clearCache();
-        
+
         cacheManager.getCss(urlSource);
         assertEquals(2, invocationOfGetContent);
     }
-    
+
+    @Test
+    public void clearCacheImportedSourcesSize() {
+        LessCacheManager cacheManager = new LessCacheManager();
+
+        URLSource urlSource = createSampleURLSource();
+
+        cacheManager.getCss(urlSource);
+        int expectedItems = urlSource.getImportedSources().size();
+
+        cacheManager.clearCache();
+
+        cacheManager.getCss(urlSource);
+        assertEquals(expectedItems, urlSource.getImportedSources().size());
+    }
+
+    @Test
+    public void timestampBeforeAndAfterCompile()
+    {
+        /*
+         * This test should not be used more than to illustrate that there is an issue with the time
+         * stamping before and after the first compilation. It may succeed or fail depending on the
+         * order where resources/root.less and resources/imported1.less was edited.
+         *
+         * If it succeeds should you edit resources/imported1.less and rerun the test, this change
+         * will also fail the test cachesCssResult.
+         */
+        LessCacheManager cacheManager = new LessCacheManager();
+
+        URLSource urlSource = createSampleURLSource();
+
+        Time expectedTime = cacheManager.getLastModifiedTime(urlSource);
+        cacheManager.getCss(urlSource);
+        assertEquals(expectedTime, cacheManager.getLastModifiedTime(urlSource));
+    }
+
     @Test
     public void usesLessCompilerConfigurationFactoryProvidedToCreateANewLesCompilerConfiguration() {
         LessCacheManager cacheManager = new LessCacheManager(new LessCompilerConfigurationFactory() {
@@ -77,26 +113,26 @@ public class LessCacheManagerTest {
                 return new Configuration();
             }
         });
-        
+
         URLSource urlSource = createSampleURLSource();
 
         cacheManager.getCss(urlSource);
         assertEquals(1, invocationOfNewConfiguration);
-        
+
         cacheManager.clearCache();
 
         cacheManager.getCss(urlSource);
         assertEquals(2, invocationOfNewConfiguration);
     }
-    
+
     @Test
     public void usesDefaultConfigurationFactoryWhenProvidingNull() {
         LessCacheManager cacheManager = new LessCacheManager(null);
-        
+
         URLSource urlSource = createSampleURLSource();
 
         cacheManager.getCss(urlSource);
-        
+
         // no NullPointerException
     }
 
